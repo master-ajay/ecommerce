e9cb5e95ee6265ac1e1097ef391cf7cbb0382ec1
DEVELOPER-AJAY <palrawatajay@gmail.com>
2023-06-24 12:49:25 +0530

    Created Responsive Header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,19 +1,91 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import Wrapper from "./Wrapper";
 import Link from "next/link";
-import "../assets/logo.svg";
+import ReactLogo from "../../public/assets/logo.svg";
+import Image from "next/image";
+import Menu from "./Menu";
+import MenuMobile from "./MenuMobile";
+import { BsCart } from "react-icons/bs";
+import { VscChromeClose } from "react-icons/vsc";
+import { BiMenuAltRight } from "react-icons/bi";
 
 export default function Header() {
+  const [mobileMenu, setMobileMenu] = useState<boolean>(false);
+  const [showCatMenu, setShowCatMenu] = useState<boolean>(false);
   const [show, setShow] = useState<string>("translate-y-0");
+  const [lastScrollY, setLastScrollY] = useState<number>(0);
+
+  const controlNavbar = () => {
+    if (window.scrollY > 200) {
+      if (window.scrollY > lastScrollY && !mobileMenu) {
+        setShow("-translate-y-[80px]");
+      } else {
+        setShow("shadow-sm");
+      }
+    } else {
+      setShow("translate-y-0");
+    }
+    setLastScrollY(window.scrollY);
+    console.log(window.scrollY);
+  };
+
+  useEffect(() => {
+    window.addEventListener("scroll", controlNavbar);
+    return () => {
+      window.removeEventListener("scroll", controlNavbar);
+    };
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [lastScrollY]);
+
   return (
     <header
-      className={`w-full h-[50px] md:h-[80px] bg-white flex items-center justify-between z-20 sticky top-0 transition-transform duration-300 ${show}`}
+      className={`w-full h-[50px] md:h-[80px] bg-white flex items-center 
+      justify-between z-20 sticky top-0 transition-transform duration-300 ${show}`}
     >
-      AJAY
-      <Wrapper>
+      <Wrapper className="h-[60px] flex justify-between items-center">
         <Link href="/">
-          <img src="src/assets/logo.svg" className="w-[40px] md:w-[60px]" />
+          <Image src={ReactLogo} alt={""} className="w-[40px] md:w-[60px]" />
         </Link>
+        <Menu showCatMenu={showCatMenu} setShowCatMenu={setShowCatMenu} />
+        {mobileMenu && (
+          <MenuMobile
+            showCatMenu={showCatMenu}
+            setShowCatMenu={setShowCatMenu}
+            setMobileMenu={setMobileMenu}
+          />
+        )}
+        <div className="flex items-center gap-2 text-black">
+          <div
+            className="w-8 md:w-12 h-8 md:h-12 rounded-full flex justify-center
+           items-center hover:bg-black/[0.05] cursor-pointer relative"
+          >
+            <BsCart className="h-[15px] md:text-[20px]" />
+            <div
+              className="h-[14px] md:h-[18px] min-w-[14px] md:min-w-[18px]
+             rounded-full bg-red-600 absolute top-1 left-5 md:left-7 text-white
+              text-[10px] md:text-[12px] flex justify-center items-center px-[2px]
+               md:px-[5px]"
+            >
+              5
+            </div>
+          </div>
+          {/* Icon end */}
+
+          {/* Mobile icon start */}
+          <div className="w-8 md:w-12 h-8 md:h-12 rounded-full flex justify-center items-center hover:bg-black/[0.05] cursor-pointer relative">
+            {mobileMenu ? (
+              <VscChromeClose
+                className="text-[16px]"
+                onClick={() => setMobileMenu(false)}
+              />
+            ) : (
+              <BiMenuAltRight
+                className="text-[20px]"
+                onClick={() => setMobileMenu(true)}
+              />
+            )}
+          </div>
+        </div>
       </Wrapper>
     </header>
   );
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,6 @@
-import { Inter } from "next/font/google";
 import Header from "@/components/Header";
 import Footer from "@/components/Footer";
 
-const inter = Inter({ subsets: ["latin"] });
-
 export default function Home() {
-  return (
-    <div>
-      <Header />
-      <Footer />
-    </div>
-  );
+  return <main className="h-[2000px]">Home Page...</main>;
 }
