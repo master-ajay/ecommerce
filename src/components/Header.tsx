38a263a00375065a9bf7353c3515e4934a368156
DEVELOPER-AJAY <palrawatajay@gmail.com>
2023-06-24 16:47:53 +0530

    Commit For Footer

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -8,6 +8,7 @@ import MenuMobile from "./MenuMobile";
 import { BsCart } from "react-icons/bs";
 import { VscChromeClose } from "react-icons/vsc";
 import { BiMenuAltRight } from "react-icons/bi";
+import { IoMdHeartEmpty } from "react-icons/io";
 
 export default function Header() {
   const [mobileMenu, setMobileMenu] = useState<boolean>(false);
@@ -55,6 +56,14 @@ export default function Header() {
           />
         )}
         <div className="flex items-center gap-2 text-black">
+          {/* Icon start */}
+          <div className="w-8 md:w-12 h-8 md:h-12 rounded-full flex justify-center items-center hover:bg-black/[0.05] cursor-pointer relative">
+            <IoMdHeartEmpty className="text-[19px] md:text-[24px]" />
+            <div className="h-[14px] md:h-[18px] min-w-[14px] md:min-w-[18px] rounded-full bg-red-600 absolute top-1 left-5 md:left-7 text-white text-[10px] md:text-[12px] flex justify-center items-center px-[2px] md:px-[5px]">
+              51
+            </div>
+          </div>
+          {/* Icon end */}
           <div
             className="w-8 md:w-12 h-8 md:h-12 rounded-full flex justify-center
            items-center hover:bg-black/[0.05] cursor-pointer relative"
@@ -72,7 +81,7 @@ export default function Header() {
           {/* Icon end */}
 
           {/* Mobile icon start */}
-          <div className="w-8 md:w-12 h-8 md:h-12 rounded-full flex justify-center items-center hover:bg-black/[0.05] cursor-pointer relative">
+          <div className="w-8 md:w-12 h-8 md:h-12 rounded-full flex justify-center items-center hover:bg-black/[0.05] cursor-pointer relative md:hidden">
             {mobileMenu ? (
               <VscChromeClose
                 className="text-[16px]"
